11774e54e6830698517e098d0a38bc006e524fab
thowell07 <tarae.howell@gmail.com>
2026-04-19 12:29:22 -0400

    Fix Pages base path for renamed repo

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/cliff-house-maine-dashboard/',
+  base: '/cliff-house-maine-dashboard-2026/',
   plugins: [react()],
 });
